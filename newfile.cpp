ec33a116c17f2a639f2770cadd86688ae5764f15
abs109 <mtenabs@gmail.com>
2025-03-11 13:38:17 +0530

    Create newfile.cpp

---
+++ newfile.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout<<"Atulya10";
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins, This is PES2UG22CS114\n";
+  cout << "I like football  \n";
+  return 0;
+}
