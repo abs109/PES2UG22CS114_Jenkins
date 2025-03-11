3c5e20183eba15d4739b3ec863bf46cebf9c8447
abs109 <mtenabs@gmail.com>
2025-03-11 13:25:44 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(void) {
-  cout<<"Atulya";
+  cout<<"Atulya10";
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
